960074bb47e971574abd51306cc244f07459943e
Félix Vicente <felixvicent1306@gmail.com>
2025-07-02 11:33:19 -0300

    Configure SES

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,tsx}'],
     plugins: { js },
     extends: ['js/recommended'],
     rules: {
---
+++ src/infra/emails/components/TailwindConfig.tsx
@@ -0,0 +1,25 @@
+import { Tailwind } from '@react-email/tailwind';
+import React from 'react';
+
+interface ITailwindConfigProps {
+  children: React.ReactNode;
+}
+
+export function TailwindConfig({ children }: ITailwindConfigProps) {
+  return (
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              foodiary: { green: '#64A30D' },
+              gray: { 600: '#A1A1A1' },
+            },
+          },
+        },
+      }}
+    >
+      {children}
+    </Tailwind>
+  );
+}
